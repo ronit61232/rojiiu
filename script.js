cef7ff27d12f45685288b0fd090066a0c6329c94
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-08-28 23:06:12 +0530

    added navbar

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Script for navigation bar
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
+}
