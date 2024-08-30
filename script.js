f6476360c1826509731a7e37bc2b14f2ee3f55d8
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-08-30 23:23:41 +0530

    update script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Script for navigation bar
+// // Script for navigation bar
 
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
+// const bar = document.getElementById('bar');
+// const nav = document.getElementById('navbar');
 
-if (bar) {
-  bar.addEventListener('click', () => {
-    nav.classList.add('active');
-  })
-}
+// if (bar) {
+//   bar.addEventListener('click', () => {
+//     nav.classList.add('active');
+//   })
+// }
